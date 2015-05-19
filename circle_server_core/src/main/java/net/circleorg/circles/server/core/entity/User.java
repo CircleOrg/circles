8d32f491d20073f9f21543460dc124acb4dae330
karlika <titotit@gmx.at>
2015-05-19 23:17:08 +0200

    circle_server_core Project setup

---
+++ circle_server_core/src/main/java/net/circleorg/circles/server/core/entity/User.java
@@ -0,0 +1,29 @@
+package net.circleorg.circles.server.core.entity;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+	private String name;
+
+    public User() {
+    	
+    }
+    
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+		return id;
+	}
+
+}
---
+++ circle_server_core/src/main/java/net/circleorg/circles/server/core/entity/mapping/UserMapping.java
@@ -0,0 +1,14 @@
+package net.circleorg.circles.server.core.entity.mapping;
+
+import net.circleorg.circles.server.core.entity.User;
+
+import org.mongolink.domain.mapper.AggregateMap;;
+
+public class UserMapping extends AggregateMap<User> {
+
+	@Override
+	public void map() {
+		id().onProperty(element().getId()).auto();
+	}
+
+}
