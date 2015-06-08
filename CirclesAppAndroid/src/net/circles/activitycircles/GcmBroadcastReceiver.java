8deaaee3695dc4d13270c193c06e1ff3c15ce41e
karlika <titotit@gmx.at>
2015-06-08 20:33:35 +0200

    project restructure

---
+++ CirclesAppAndroid/src/net/circles/activitycircles/GcmBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package net.circles.activitycircles;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ CirclesAppAndroid/src/net/circles/activitycircles/GcmIntentService.java
@@ -0,0 +1,15 @@
+package net.circles.activitycircles;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class GcmIntentService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
